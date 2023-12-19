664e59a272e2f0ea4f5436dee295c327950c535b
mygit@23 <naveenmadhesan23@gmail.com>
2023-12-19 14:39:07 +0530

    Add project files.

---
+++ C#basicPracticeExc/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_basicPracticeExc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // PrimitiveTypesExpressions.Doit();
+            ControlFlow.SwitchLoop();
+            Console.Read();
+        }
+    }
+}
